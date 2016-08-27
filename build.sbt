8443e51b5600b1b8b0c8421610b279269bd91640
Tamas Martin <tamas.martin@google.com>
2016-08-27 12:26:13 +0200

    test added

--- build.sbt
+++ build.sbt
@@ -6,6 +6,6 @@ scalaVersion := "2.11.8"
 
 libraryDependencies += "org.scala-lang" % "scala-library" % "2.11.8"
 
-libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.0" % test
+libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.0"
 
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.0" % "test"
--- src/test/scala/hu/tmartin/calc/TomCalcTest.scala
+++ src/test/scala/hu/tmartin/calc/TomCalcTest.scala
@@ -1,8 +1,20 @@
 package hu.tmartin.calc
 
+import org.scalatest.{FunSpec, FunSuite, Matchers}
+
 /**
   * Created by Tamás on 2016-08-27.
   */
-class TomCalcTest {
+class TomCalcTest extends FunSuite with Matchers {
+
+  test("Testing the add function"){
+    val c = new TomCalc
+    c.add(3,4) shouldBe(7)
+  }
+
+  test("Testing the minus function"){
+    val c = new TomCalc
+    c.minus(3,4) shouldBe(-1)
+  }
 
 }
