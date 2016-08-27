827984eb75765e2692fe104948762f9dc1c821b8
Tamas Martin <tamas.martin@google.com>
2016-08-27 12:16:18 +0200

    First comit

---
+++ src/main/scala/hu/tmartin/calc/TomCalc.scala
@@ -0,0 +1,16 @@
+package hu.tmartin.calc
+
+/**
+  * Created by Tamás on 2016-08-27.
+  */
+class TomCalc {
+
+  def add(a: Int, b: Int): Int = {
+    a + b
+  }
+
+  def minus(a: Int, b: Int): Int = {
+    a - b
+  }
+
+}
---
+++ src/test/scala/hu/tmartin/calc/TomCalcTest.scala
@@ -0,0 +1,8 @@
+package hu.tmartin.calc
+
+/**
+  * Created by Tamás on 2016-08-27.
+  */
+class TomCalcTest {
+
+}
